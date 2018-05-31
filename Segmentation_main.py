314cc936b1ae7b28ce769a005490a8a131e18f1d
Simon Graham <simon_graham@hotmail.co.uk>
2018-05-31 12:06:39 +0300

    changed main

--- Segmentation_main.py
+++ Segmentation_main.py
@@ -82,7 +82,6 @@ def main(argv):
     # Testing
   elif argv[0] == 'test':
     #model_path = Segmentation_train.find_optim_net()
-
     model_path = FLAGS.checkpoint_dir + '/model.ckpt-172198'
     path_id = os.path.basename(model_path)
     path_id = path_id.split('-')[1]
