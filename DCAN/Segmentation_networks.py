44c0418f61f3010c840717db1216933662caa315
Simon Graham <simon_graham@hotmail.co.uk>
2018-06-05 10:05:49 +0100

    Update Segmentation_networks.py

--- DCAN/Segmentation_networks.py
+++ DCAN/Segmentation_networks.py
@@ -56,34 +56,44 @@ def dcan(images, keep_prob):
   with tf.variable_scope('conv13') as scope:
     conv13 = layers.conv2D_dilated(conv12, 512, 512, rate=2)
     pool5 = layers.maxPool(conv13, 3, 1, name='pool5')
-    pool6 = layers.avgPool(pool5, 3, 1, name='pool6')
 
   with tf.variable_scope('fullyConnected') as scope:
-    fc1 = layers.conv2D_dilated(pool6, 512, 1024, rate=4)
-    drop1 = tf.nn.dropout(fc1, keep_prob)
-    fc2 = layers.conv2D(drop1, 1024, 1024, kernel_size = (1,1), name_ext='_2')
-    drop2 = tf.nn.dropout(fc2, keep_prob)
+    fc1 = layers.conv2D_dilated(pool5, 512, 1024, rate=4)
+    fc2 = layers.conv2D_dilated(fc1, 1024, 1024, rate=4, name_ext='_2')
+    fc2 = tf.nn.dropout(fc2, keep_prob)
 
   with tf.variable_scope('output') as scope:
-    fc_output = layers.conv2D(drop2, 1024, FLAGS.n_classes)
-    block5_output = layers.conv2D(conv13, 512, FLAGS.n_classes, name_ext='_2')
-    block4_output = layers.conv2D(conv10, 512, FLAGS.n_classes, name_ext='_3')
+    output1 = layers.conv2D(fc2, 1024, 128, kernel_size=(1,1))
+    output2 = layers.conv2D(conv13, 512, 128, kernel_size=(1,1), name_ext='_2')
+    output3 = layers.conv2D(conv10, 512, 128, kernel_size=(1,1), name_ext='_3')
 
   with tf.variable_scope('upSample_gland') as scope:
-    upSample_gland1 = layers.upSample2D(fc_output, FLAGS.n_classes, 
-      FLAGS.n_classes, upscale_factor=8, initializer='bilinear')
-    upSample_gland2 = layers.upSample2D(block5_output, FLAGS.n_classes, 
-      FLAGS.n_classes, upscale_factor=8, name_ext='_2', initializer='bilinear')
-    upSample_gland3 = layers.upSample2D(block4_output, FLAGS.n_classes, 
-      FLAGS.n_classes, upscale_factor=8, name_ext='_3', initializer='bilinear')
+    upSample_gland1 = layers.upSample2D(output1, 128, 
+      128, upscale_factor=8)
+    upSample_gland1 = layers.conv2D(upSample_gland1, 128, 128)
+    upSample_gland1 = layers.conv2D(upSample_gland1, 128, FLAGS.n_classes, name_ext='_2', activation=False)
+    upSample_gland2 = layers.upSample2D(output2, 128, 
+      128, upscale_factor=8, name_ext='_2')
+    upSample_gland2 = layers.conv2D(upSample_gland2, 128, 128, name_ext='_3')
+    upSample_gland2 = layers.conv2D(upSample_gland2, 128, FLAGS.n_classes, activation=False, name_ext='_4')
+    upSample_gland3 = layers.upSample2D(output3, 128, 
+      128, upscale_factor=8, name_ext='_3')
+    upSample_gland3 = layers.conv2D(upSample_gland3, 128, 128, name_ext='_5')
+    upSample_gland3 = layers.conv2D(upSample_gland3, 128, FLAGS.n_classes, activation=False, name_ext='_6')
 
   with tf.variable_scope('upSample_contour') as scope:
-    upSample_contour1 = layers.upSample2D(fc_output, FLAGS.n_classes, 
-      FLAGS.n_classes, upscale_factor=8, initializer='bilinear')
-    upSample_contour2 = layers.upSample2D(block5_output, FLAGS.n_classes, 
-      FLAGS.n_classes, upscale_factor=8, name_ext='_2', initializer='bilinear')
-    upSample_contour3 = layers.upSample2D(block4_output, FLAGS.n_classes, 
-      FLAGS.n_classes, upscale_factor=8, name_ext='_3', initializer='bilinear')
+    upSample_contour1 = layers.upSample2D(output1, 128, 
+      128, upscale_factor=8)
+    upSample_contour1 = layers.conv2D(upSample_contour1, 128, 128)
+    upSample_contour1 = layers.conv2D(upSample_contour1, 128, FLAGS.n_classes, name_ext='_2', activation=False)
+    upSample_contour2 = layers.upSample2D(output2, 128, 
+      128, upscale_factor=8, name_ext='_2')
+    upSample_contour2 = layers.conv2D(upSample_contour2, 128, 128, name_ext='_3')
+    upSample_contour2 = layers.conv2D(upSample_contour2, 128, FLAGS.n_classes, activation=False, name_ext='_4')
+    upSample_contour3 = layers.upSample2D(output3, 128, 
+      128, upscale_factor=8, name_ext='_3')
+    upSample_contour3 = layers.conv2D(upSample_contour3, 128, 128, name_ext='_5')
+    upSample_contour3 = layers.conv2D(upSample_contour3, 128, FLAGS.n_classes, activation=False, name_ext='_6')
 
   with tf.variable_scope('softmax_gland') as scope:
     softmax_gland1 = layers.softMax(upSample_gland1)
